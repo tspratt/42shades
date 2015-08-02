268d903f5fdd929778413e7a0305b2e091fb319d
Tracy <tspratt@gmail.com>
2015-08-02 17:58:09 -0400

    Implemented list Browser

--- public/controllers/member-list.js
+++ public/controllers/member-list.js
@@ -3,10 +3,58 @@ angular.module('club42')
     function($scope, $state, appConstants, memberData) {
       $scope.members = [];
       $scope.member = {};
+      $scope.pageNum = 0;
+      $scope.pageLen = 10;
 
       function initModule(){
-
+        getMembers();
       }
+
+      $scope.nextPage = function (){
+        console.log('nextPage');
+        $scope.pageNum ++;
+        getMembers();
+      };
+      $scope.prevPage = function (){
+        console.log('prevPage');
+        if ($scope.pageNum > 0) {
+          $scope.pageNum --;
+          getMembers();
+        }
+      };
+      $scope.firstPage = function (){
+        console.log('firstPage');
+        $scope.pageNum = 0;
+        getMembers();
+      };
+      $scope.lastPage = function (){
+
+      };
+
+      $scope.onChangePageLen = function(){
+        console.log('onChangePageLen');
+        getMembers();
+      };
+
+      function getMembers () {
+        $scope.member = null;
+        memberData.getMembers($scope.pageNum,$scope.pageLen,null,null, false)
+          .then(function (res) {
+            if (res.status >= 200 && res.status < 300) {
+              $scope.members = res.data.data;
+            }
+            else {
+              console.log('HTTP Error: ' + res.statusText);
+            }
+
+          });
+      }
+
+
+      $scope.onClickMember = function(oMember) {
+        $scope.member = oMember;
+        $scope.selectedId = oMember._id
+      };
 
       initModule();
 
---
+++ public/controllers/paginator.js
@@ -0,0 +1,31 @@
+angular.module('club42')
+  .controller('Paginator', ['$scope', '$state', 'appConstants', 'memberData',
+    function($scope, $state, appConstants, memberData) {
+      $scope.pageNum = 10;
+      $scope.pageLen = 10;
+
+      $scope.nextPage = function (){
+        console.log('nextPage');
+      };
+      $scope.prevPage = function (){
+        console.log('prevPage');
+      };
+      $scope.firstPage = function (){
+        console.log('firstPage');
+      };
+      $scope.lastPage = function (){
+
+      };
+
+      $scope.onChangePageLen = function(){
+        console.log('onChangePageLen');
+      }
+
+}])
+  .directive('paginator', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'partials/paginator.html'
+    }
+  });
